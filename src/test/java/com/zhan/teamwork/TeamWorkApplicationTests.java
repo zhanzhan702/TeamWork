a0200145280488af09ece76015ddf8761cbf02b7
zhanzhan <2837420590@qq.com>
2026-05-06 17:00:19 +0800

    初始化

---
+++ src/test/java/com/zhan/teamwork/TeamWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhan.teamwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
